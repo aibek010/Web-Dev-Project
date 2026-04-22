333014a8a975ab6c64c639de8a723a8c93853d26
aibek010 <a_doskeyev@kbtu.kz>
2026-04-22 21:49:18 +0500

    Register models in Django admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category,Transaction,Budget,FinancialGoal
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Transaction)
+admin.site.register(Budget)
+admin.site.register(FinancialGoal)
